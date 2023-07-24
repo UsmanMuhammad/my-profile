3305ad7e6472a528fcc446b73714e78946a2131a
Muhammad Usman <bcs.12.16@gmail.com>
2023-07-24 22:32:38 +0200

    Add basic info

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        sans: ['PT Sans', 'sans-serif'],
+        body: ['"Open Sans"']
+      },
     },
   },
   plugins: [],
